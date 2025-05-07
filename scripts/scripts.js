44db2f2a3d6fee34f4606f9648dda12beb3a5965
offline-pixel <d.ranolia92@gmail.com>
2025-05-08 00:49:05 +0530

    abo sss

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,11 @@
 // scripts.js
-
-// Adjusted to dynamically resolve relative path
-const basePath = window.location.pathname.includes('/pages/') ? '../sections/' : './sections/';
-
 async function loadIncludes() {
   const [header, footer] = await Promise.all([
-    fetch(`${basePath}header.html`).then(res => res.text()),
-    fetch(`${basePath}footer.html`).then(res => res.text())
+    fetch('./sections/header.html').then(res => res.text()),
+    fetch('./sections/footer.html').then(res => res.text())
   ]);
   document.body.insertAdjacentHTML('afterbegin', header);
   document.body.insertAdjacentHTML('beforeend', footer);
 }
-
 
 document.addEventListener('DOMContentLoaded', loadIncludes);
