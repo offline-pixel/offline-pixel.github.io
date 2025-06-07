63f476140d0062aeb1d4f234953ef15f8cb7c326
offline-pixel <d.ranolia92@gmail.com>
2025-06-07 11:18:49 +0530

    gtmsssss

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,9 @@
 // scripts.js
 async function loadIncludes() {
   const [headContent, header, footer] = await Promise.all([
-    // fetch('./sections/head-common.html').then(res => res.text()),
     fetch('./sections/header.html').then(res => res.text()),
     fetch('./sections/footer.html').then(res => res.text())
   ]);
-  // document.head.insertAdjacentHTML('afterbegin', headContent);
 
   document.body.insertAdjacentHTML('afterbegin', header);
   document.body.insertAdjacentHTML('beforeend', footer);
